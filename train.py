fee76165bc0617bbba62ff71a3d2e262e350925d
HuangYin <1071324110@qq.com>
2021-08-23 15:27:57 +0800

    benchmark

--- train.py
+++ train.py
@@ -1,6 +1,16 @@
+import torch
 from options.train_options import TrainOptions
 
-
 if __name__ == '__main__':
+    # opt ==============================================================================
     opt = TrainOptions().parse()
-    print(opt)
+
+    # Fix random seed==============================================================================
+    torch.manual_seed(1)
+    torch.cuda.manual_seed_all(1)
+
+    # speed up compution==============================================================================
+    torch.backends.cudnn.benchmark = True
+
+    # data============================================================================================================
+    
