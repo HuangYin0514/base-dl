98504de3f56421e4320c999c45316ba0459b9e08
HuangYin <1071324110@qq.com>
2021-08-23 10:19:06 +0800

    opt

---
+++ train-cifar10.py
@@ -0,0 +1,8 @@
+import time
+from options.train_options import TrainOptions
+# from data import create_dataset
+# from models import create_model
+# from util.visualizer import Visualizer
+
+if __name__ == '__main__':
+    opt = TrainOptions().parse()  
