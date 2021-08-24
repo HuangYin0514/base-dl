3948a3f2dc083c8a5bc9437717c538c709de6693
HuangYin <1071324110@qq.com>
2021-08-24 17:51:11 +0800

    clean

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
+import os
 import torch
+
 from options.train_options import TrainOptions
 import torchvision.transforms as transforms
+from data.cat_dog import CatAndDog
 
 if __name__ == '__main__':
     # opt ==============================================================================
@@ -41,3 +44,25 @@ if __name__ == '__main__':
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor()
         ])
+
+
+    import glob
+
+    train_dir = './datasets/dogVScat/train'
+    test_dir = 'datasets/dogVScat/test'
+    batch_size=2
+
+    print(os.listdir(train_dir)[:5])
+
+    train_list = glob.glob(os.path.join(train_dir,'*.jpg'))
+    test_list = glob.glob(os.path.join(test_dir, '*.jpg'))
+
+    train_data = CatAndDog(train_list, transform=train_transforms)
+    test_data = CatAndDog(test_list, transform=test_transforms)
+
+
+    train_loader = torch.utils.data.DataLoader(dataset = train_data, batch_size=batch_size, shuffle=True )
+    test_loader = torch.utils.data.DataLoader(dataset = test_data, batch_size=batch_size, shuffle=True)
+    print(train_loader)
+
+    
