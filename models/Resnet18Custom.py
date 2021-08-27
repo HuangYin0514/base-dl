b40d63bbd93bb7ff0d1c8c68d1b8f22eb4da00cd
HuangYin <1071324110@qq.com>
2021-08-27 16:58:15 +0800

    clean

--- models/Resnet18Custom.py
+++ models/Resnet18Custom.py
@@ -7,10 +7,9 @@ class Resnet18Custom(nn.Module):
     def __init__(self):
         super(Resnet18Custom, self).__init__()
 
-        self.model  = models.resnet18(pretrained=True)
+        self.model = models.resnet18(pretrained=True)
         num_features = self.model.fc.in_features
-        self.model.fc = nn.Linear(num_features,2)
-
+        self.model.fc = nn.Linear(num_features, 2)
 
     def forward(self, x):
         x = self.model(x)
