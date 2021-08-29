f563596811301efc944a56cea77ecf5b15c3c07f
HuangYin <1071324110@qq.com>
2021-08-29 23:38:30 +0800

    random_seed

--- train_old.py
+++ train_old.py
@@ -26,6 +26,8 @@ parser.add_argument("--test_dir", type=str, default="./datasets/hymenoptera_data
 parser.add_argument("--batch_size", default=128, type=int)
 parser.add_argument("--test_batch_size", default=128, type=int)
 parser.add_argument("--num_workers", default=0, type=int)
+# Optimizer
+parser.add_argument("--lr", type=float, default=0.1)
 # train
 parser.add_argument("--num_epochs", type=int, default=2)
 
@@ -53,19 +55,14 @@ curve = draw_curve.Draw_Curve(save_dir_path)
 # data Augumentation
 train_transforms = T.Compose(
     [
-        # T.Resize(((opt.img_height, opt.img_width)), interpolation=3),
-        # T.RandomResizedCrop((opt.img_height, opt.img_width)),
-        T.RandomResizedCrop(224),
-        T.RandomHorizontalFlip(),
+        T.Resize(((opt.img_height, opt.img_width)), interpolation=3),
         T.ToTensor(),
         T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
     ]
 )
 test_transforms = T.Compose(
     [
-        # T.Resize(((opt.img_height, opt.img_width)), interpolation=3),
-        T.Resize(256),
-        T.CenterCrop(224),
+        T.Resize(((opt.img_height, opt.img_width)), interpolation=3),
         T.ToTensor(),
         T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
     ]
@@ -91,11 +88,10 @@ if device == "cuda":
 criterion = F.cross_entropy
 
 # optimizer ============================================================================================================
-# optimizer = optim.Adam(params=model.parameters(), lr=opt.lr)
-optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+optimizer = optim.Adam(params=model.parameters(), lr=opt.lr)
 
 # scheduler ============================================================================================================
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
 
 
 # Training and test ============================================================================================================
@@ -140,7 +136,7 @@ def train():
             time_remaining = (
                 (opt.num_epochs - epoch) * (time.time() - start_time) / (epoch + 1)
             )
-
+            
             logger.info(
                 "Epoch:{}/{} \tTrain Loss:{:.4f} \tAcc:{:.4f} \tETA:{:.0f}h{:.0f}m".format(
                     epoch + 1,
