1fa3445bb55498239b35416b8e774a58c004b943
HuangYin <1071324110@qq.com>
2021-09-02 14:33:03 +0800

    model

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 
 import numpy as np
+import torch
 from models import *
 
 if __name__ == '__main__':
-    model = resnet50(pretrained=True)    
+    model = Resnet_Classification()    
+    inputs = torch.randn(3,3,224,224)
+    outputs = model(inputs)
     print(model)
+    print(outputs)
