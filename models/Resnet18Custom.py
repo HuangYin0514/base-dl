281b43f3accbf80ff739736492fb09a183834ccc
HuangYin <1071324110@qq.com>
2021-09-03 22:57:50 +0800

    resnet18

--- models/Resnet18Custom.py
+++ models/Resnet18Custom.py
@@ -15,27 +15,27 @@ model_urls = {
 }
 
 
-def weights_init_kaiming(m):
-    classname = m.__class__.__name__
-    if classname.find("Linear") != -1:
-        nn.init.kaiming_normal_(m.weight, a=0, mode="fan_out")
-        nn.init.constant_(m.bias, 0.0)
-    elif classname.find("Conv") != -1:
-        nn.init.kaiming_normal_(m.weight, a=0, mode="fan_in")
-        if m.bias is not None:
-            nn.init.constant_(m.bias, 0.0)
-    elif classname.find("BatchNorm") != -1:
-        if m.affine:
-            nn.init.normal_(m.weight, 1.0, 0.02)
-            nn.init.constant_(m.bias, 0.0)
+# def weights_init_kaiming(m):
+#     classname = m.__class__.__name__
+#     if classname.find("Linear") != -1:
+#         nn.init.kaiming_normal_(m.weight, a=0, mode="fan_out")
+#         nn.init.constant_(m.bias, 0.0)
+#     elif classname.find("Conv") != -1:
+#         nn.init.kaiming_normal_(m.weight, a=0, mode="fan_in")
+#         if m.bias is not None:
+#             nn.init.constant_(m.bias, 0.0)
+#     elif classname.find("BatchNorm") != -1:
+#         if m.affine:
+#             nn.init.normal_(m.weight, 1.0, 0.02)
+#             nn.init.constant_(m.bias, 0.0)
 
 
-def weights_init_classifier(m):
-    classname = m.__class__.__name__
-    if classname.find("Linear") != -1:
-        nn.init.normal_(m.weight, std=0.001)
-        if m.bias:
-            nn.init.constant_(m.bias, 0.0)
+# def weights_init_classifier(m):
+#     classname = m.__class__.__name__
+#     if classname.find("Linear") != -1:
+#         nn.init.normal_(m.weight, std=0.001)
+#         if m.bias:
+#             nn.init.constant_(m.bias, 0.0)
 
 
 def conv3x3(in_planes, out_planes, stride=1):
@@ -50,41 +50,39 @@ def conv1x1(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
 
+# resnet18等网络
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
         super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(
-            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False
-        )
+        self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(
-            planes, planes, kernel_size=3, stride=1, padding=1, bias=False
-        )
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
         self.bn2 = nn.BatchNorm2d(planes)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion * planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(
-                    in_planes,
-                    self.expansion * planes,
-                    kernel_size=1,
-                    stride=stride,
-                    bias=False,
-                ),
-                nn.BatchNorm2d(self.expansion * planes),
-            )
+        self.downsample = downsample
+        self.stride = stride
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
         return out
 
-
+# resnet50 等网络
 class Bottleneck(nn.Module):
     expansion = 4
 
@@ -191,6 +189,22 @@ class ResNet(nn.Module):
         return x
 
 
+
+def resnet18(pretrained=False):
+    """Constructs a ResNet-18 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(BasicBlock, [2, 2, 2, 2])
+    if pretrained:
+        pretrained_state_dict = model_zoo.load_url(model_urls["resnet18"])
+        now_state_dict = model.state_dict()
+        now_state_dict.update(pretrained_state_dict)
+        model.load_state_dict(now_state_dict)
+    return model
+
+
+
 def resnet50(pretrained=False):
     """Constructs a ResNet-50 model.
     Args:
@@ -211,7 +225,7 @@ class Resnet_Backbone(nn.Module):
 
         # backbone--------------------------------------------------------------------------
         # change the model different from pcb
-        resnet = resnet50(pretrained=True)
+        resnet = resnet18(pretrained=True)
         # Modifiy the stride of last conv layer----------------------------
         # resnet.layer4[0].downsample[0].stride = (1, 1)
         # resnet.layer4[0].conv2.stride = (1, 1)
